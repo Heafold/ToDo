518c8f61d93f97daf452431009fea7050d8565a7
Corentin Maese <corentinmaese@MBP-de-Corentin.numericable.fr>
2023-01-13 11:59:47 +0100

    formulaire-integration

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('js'));
 app.use(express.static('dist'));
 
 app.set('view engine', 'pug');
